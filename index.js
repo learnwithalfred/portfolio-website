7acb3d75c4ee67eff00b9c1e32d13712a320eb99
Alfred Boateng <programmingwithalfred@gmail.com>
2022-05-08 17:58:04 +0000

    Fix issue with nav background

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const hamburger = document.querySelector('.hamburger');
 const navMenu = document.querySelector('.nav-menu');
+const header = document.querySelector('header');
+const body = document.querySelector('body');
+
 hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('active');
   navMenu.classList.toggle('active');
+  header.classList.add('h-100');
+  body.classList.add('no-scroll-bg');
 });
 
 document.querySelectorAll('.nav-link').forEach((item) => {
@@ -10,5 +15,7 @@ document.querySelectorAll('.nav-link').forEach((item) => {
     console.log('i am clicked');
     hamburger.classList.remove('active');
     navMenu.classList.remove('active');
+    header.classList.remove('h-100');
+    body.classList.remove('no-scroll-bg');
   });
 });
