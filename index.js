f30e6556204dce22281cb1c36c01a8fc32c70c5f
Alfred Boateng <programmingwithalfred@gmail.com>
2022-05-08 19:37:05 +0000

    Remove mail icon on small screens

--- index.js
+++ index.js
@@ -2,20 +2,21 @@ const hamburger = document.querySelector('.hamburger');
 const navMenu = document.querySelector('.nav-menu');
 const header = document.querySelector('header');
 const body = document.querySelector('body');
+// const logo = document.querySelector('nav__logo');
 
 hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('active');
   navMenu.classList.toggle('active');
-  header.classList.add('h-100');
+  header.classList.add('full-height');
   body.classList.add('no-scroll-bg');
+  // logo.classList.toggle('d-none');
 });
 
 document.querySelectorAll('.nav-link').forEach((item) => {
   item.addEventListener('click', () => {
-    console.log('i am clicked');
     hamburger.classList.remove('active');
     navMenu.classList.remove('active');
-    header.classList.remove('h-100');
+    header.classList.remove('full-height');
     body.classList.remove('no-scroll-bg');
   });
 });
