cd3ecddbbaf7a293adf147cd3f0214e56c090205
Alfred Boateng <programmingwithalfred@gmail.com>
2022-05-09 09:11:59 +0000

    Move js files to js folder

--- js/menu.js
+++ js/menu.js
@@ -2,14 +2,14 @@ const hamburger = document.querySelector('.hamburger');
 const navMenu = document.querySelector('.nav-menu');
 const header = document.querySelector('header');
 const body = document.querySelector('body');
-// const logo = document.querySelector('nav__logo');
+const logo = document.querySelector('#nav__logo');
 
 hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('active');
   navMenu.classList.toggle('active');
   header.classList.add('full-height');
   body.classList.add('no-scroll-bg');
-  // logo.classList.toggle('d-none');
+  logo.classList.add('d-none');
 });
 
 document.querySelectorAll('.nav-link').forEach((item) => {
